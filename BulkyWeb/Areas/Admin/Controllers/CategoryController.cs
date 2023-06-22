395c178e92fb782ea1795f0c8aa067d84737cb16
Mathews Alves <mathewsalves1@outlook.com>
2023-06-22 14:36:34 +0100

    Category CRUD done, started to implement Product CRUD

---
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,110 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category Name cannot be the same as Display Order");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? objCategory = _unitOfWork.Category.Get(c => c.Id == id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+            return View(objCategory);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category Name cannot be the same as Display Order");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? objCategory = _unitOfWork.Category.Get(c => c.Id == id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+            return View(objCategory);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? objCategory = _unitOfWork.Category.Get(c => c.Id == id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Category.Remove(objCategory);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Category != null)
+            {
+                Category = await _context.Category.ToListAsync();
+            }
+        }
+    }
+}
